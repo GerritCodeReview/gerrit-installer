67b7aed01e679ed91176f5b5ad01f1e0327d6895
Luca Milanesio <luca.milanesio@gmail.com>
2015-01-17 11:16:47 +0000

    [WIP] Initial FPM-based RPM/DEB scheleton for Gerrit minimal install
    Gerrit is installed with a self-contained config with development_become_any_account
    authentication. This allows to start playing with Gerrit with zero-config effort
    needed.
    
    Gerrit site is on /var/gerrit.
    
    Service is installed on /etc/init.d/gerrit and can be started/stopped with:
    /etc/init.d/gerrit start
    /etc/init.d/gerrit stop
    
    Gerrit configuration files are on /etc/gerrit and /etc/default/gerritcodereview
    whilst logs are generated on /var/log/gerrit
    
    Change-Id: Idc270101bb27178c6e1062acb31f74425931087c

---
+++ fpm/scripts/after-install.sh
@@ -0,0 +1,9 @@
+GERRIT_SITE=/var/gerrit
+SUDO="sudo -u gerrit"
+$SUDO java -jar /var/gerrit/bin/gerrit.war init --batch -d $GERRIT_SITE
+$SUDO java -jar /var/gerrit/bin/gerrit.war reindex -d $GERRIT_SITE
+cd /etc/init.d && ln -sf $GERRIT_SITE/bin/gerrit.sh gerrit
+cd /var/log && ln -sf $GERRIT_SITE/logs gerrit
+cd /etc && ln -sf $GERRIT_SITE/etc gerrit
+[ ! -d /etc/default ] && mkdir -p /etc/default  
+cd /etc/default && ln -sf /etc/gerrit/gerritcodereview 
