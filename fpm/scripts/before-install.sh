6680b191db7f440b97cf0c84928fb1b406421762
Luca Milanesio <luca.milanesio@gmail.com>
2015-01-21 16:57:13 +0000

    Enable custom-built packages (user/group/location)
    Automatically replace the USER, GROUP, GERRIT_SITE, VERSION
    during the packaging phase and on configuration files in
    order to create a fully automated custom-built package.
    
    Example: your company requires Gerrit 2.9.3 to be installed on
    /opt/gerrit under the user 'review' and group 'wheel'.
    
    $ make USER=review GROUP=wheel GERRIT_SITE=/opt/gerrit VERSION=2.9.3
    
    Generates a package (rpm/deb) named:
    out/gerrit-2.9.3-1.noarch.(rpm|deb)
    
    Once installed, the gerrit package info is:
    
    Name        : gerrit
    Arch        : x86_64
    Version     : 2.9.3
    Release     : 1
    Size        : 39 M
    Repo        : installed
    Summary     : Gerrit Code Review
    URL         : https://code.google.com/p/gerrit/
    Licence     : Apache 2.0
    Description : Gerrit Code Review
    
    Change-Id: Ib64688fac867ff9bb069d6f7e5ab637c81b7e4e4

--- fpm/scripts/before-install.sh
+++ fpm/scripts/before-install.sh
@@ -1,11 +1,31 @@
-GERRIT_SITE=/var/gerrit
-grep -q gerrit /etc/passwd || echo -n "Creating Gerrit user ... " && useradd gerrit -d $GERRIT_SITE --create-home && echo "DONE"
+LOG=/tmp/gerrit-installer.log
+
+GROUP_ID=$(/usr/bin/getent group $GROUP | cut -d ':' -f 3 2>> $LOG || true)
+if [ "$GROUP_ID" == "" ]
+then
+  echo -n "Creating $GROUP group ... " 
+  groupadd $GROUP >> $LOG 2>> $LOG
+  [ $? != 0 ] && echo "FAILED" && exit 1
+  GROUP_ID=$(/usr/bin/getent group $GROUP | cut -d ':' -f 3 2>> $LOG || true)
+  echo "DONE"
+fi
+
+USER_PASSWD=$(/usr/bin/getent passwd $USER 2>> $LOG || true)
+if [ "$USER_PASSWD" == "" ]
+then
+  echo -n "Creating $USER user ... " 
+  useradd $USER -M -g $GROUP_ID -d $GERRIT_SITE >> $LOG 2>> $LOG
+  [ $? != 0 ] && echo "FAILED" && exit 1
+  echo "DONE"
+fi
+
 
 echo -n "Checking Java version ... "
 JAVA_VERSION=$(java -version 2>&1)
-[ $? != 0 ] && echo -e "NOT FOUND\nPlease install Java >= 1.7.0 and try again" && exit 1
+[ $? != 0 ] && echo -e "NOT FOUND\nPlease install Java >= 1.7.0 and try again" && exit 2
 
 VERSION=`expr "$JAVA_VERSION" : '.*"\(1.[0-9\.]*\)["_]'`
 echo "$VERSION"
-test "$VERSION" "<" "1.7" && echo "ERROR: java >= 1.7.0 required by Gerrit" || true
-test "$VERSION" ">" "1.7"
+test "$VERSION" "<" "1.7" && echo "ERROR: java >= 1.7.0 required by Gerrit" && exit 3
+exit 0
+
