865cd27b9530349705fc7d36e42ef64e901145b6
Luca Milanesio <luca.milanesio@gmail.com>
2015-01-25 11:37:44 +0000

    Manages upgrades between versions
    When upgrading Gerrit we need to be careful of what we
    do in the before/after install scripts in order to avoid
    removing links after upgrade.
    
    Change-Id: I91715bf3db895518d101143f566adc0ebe1c19cb

--- fpm/scripts/after-remove.sh
+++ fpm/scripts/after-remove.sh
@@ -1,5 +1,7 @@
-rm -f /etc/init.d/gerrit || true
-rm -f /var/log/gerrit || true
-rm -f /etc/gerrit || true
-rm /etc/default/gerritcodereview || true
-userdel $USER || true
+if [ "$1" != "1" ] # Don't execute during upgrade
+then
+  rm -f /etc/init.d/gerrit || true
+  rm -f /var/log/gerrit || true
+  rm -f /etc/gerrit || true
+  rm /etc/default/gerritcodereview || true
+fi
--- fpm/scripts/before-remove.sh
+++ fpm/scripts/before-remove.sh
@@ -1,1 +1,11 @@
-$GERRIT_SITE/bin/gerrit.sh stop || true
+if [ -e /etc/init.d/gerrit ]
+then
+  GERRIT_PID=$(ps -o pid,command -u $USER | grep gerrit | awk '{print $1}')
+  if [ "$GERRIT_PID" != "" ]
+  then
+    echo -n "Stopping Gerrit (pid=$GERRIT_PID) ..."
+    /etc/init.d/gerrit stop >> /dev/null 2>> /dev/null || true
+    echo "DONE" # Ignore failures as we wanted to remove it anyway
+  fi
+fi
+
