00e333822e0abb665a2ccdd81b1d9c1b40e1fb92
Luca Milanesio <luca.milanesio@gmail.com>
2015-01-25 11:37:44 +0000

    Manages upgrades between versions
    When upgrading Gerrit we need to be careful of what we
    do in the before/after install scripts in order to avoid
    removing links after upgrade.
    
    Change-Id: I91715bf3db895518d101143f566adc0ebe1c19cb

--- fpm/scripts/before-install.sh
+++ fpm/scripts/before-install.sh
@@ -28,5 +28,18 @@ JAVA_VERSION=$(java -version 2>&1)
 VERSION=`expr "$JAVA_VERSION" : '.*"\(1.[0-9\.]*\)["_]'`
 echo "$VERSION"
 test "$VERSION" "<" "1.7" && echo "ERROR: java >= 1.7.0 required by Gerrit" && exit 3
-exit 0
 
+# Script is invoked even before upgrade, we need to stop Gerrit if active
+if [ -e /etc/init.d/gerrit ]
+then
+  GERRIT_PID=$(ps -o pid,command -u $USER | grep gerrit | awk '{print $1}')
+  if [ "$GERRIT_PID" != "" ]
+  then
+    echo -n "Stopping Gerrit (pid=$GERRIT_PID) ..."
+    /etc/init.d/gerrit stop >> $LOG 2>> $LOG
+    [ $? != 0 ] && echo "FAILED" && exit 1
+    echo "DONE"
+  fi
+fi
+
+exit 0
