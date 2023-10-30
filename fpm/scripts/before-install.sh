869b975cd2159129eb1585661da5b2d38a06989e
Jacek Centkowski <geminica.programs@gmail.com>
2023-10-30 08:55:57 +0100

    chore: improve the JDK detection
    So that it can detect any version from 10 to 99 (if it ever happens).
    Note that gerrit.war built in CI requires JDK 17 hence that evrsion is
    specified.
    
    It was verified with the following version strings:
    * openjdk version "11.0.21" 2023-10-17
    * openjdk version "17.0.9" 2023-10-17
    * openjdk version "21.0.1" 2023-10-17
    * openjdk version "22-ea" 2024-03-19
    
    Change-Id: I5af92ebaab09cc9971ee06cdf1ec4aa921448e8f

--- fpm/scripts/before-install.sh
+++ fpm/scripts/before-install.sh
@@ -24,11 +24,11 @@ fi
 
 echo -n "Checking Java version ... "
 JAVA_VERSION=$(java -version 2>&1)
-[ $? != 0 ] && echo -e "NOT FOUND\nPlease install Java 11 and try again" && exit 2
+[ $? != 0 ] && echo -e "NOT FOUND\nPlease install Java 17 and try again" && exit 2
 
-VERSION=`expr "$JAVA_VERSION" : '.*"\(17\..*\)["_]'`
+VERSION=`expr "$JAVA_VERSION" : '.*"\([1-9][0-9][\.-].*\)["_]' | cut -d "." -f1 | cut -d "-" -f1`
 echo "$VERSION"
-test "$VERSION" "<" "17" && echo "ERROR: Java 17 required by Gerrit" && exit 3
+test "$VERSION" "!=" "17" && echo "ERROR: Java 17 is required by Gerrit" && exit 3
 
 # Script is invoked even before upgrade, we need to stop Gerrit if active
 GERRIT_PID=$(ps -o pid,command -u $USER | grep gerrit | awk '{print $1}')
